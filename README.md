c5ee76d1a9f206f14636777c5fe189d581098665
Emma Velazquez <emmasiren24@gmail.com>
2022-11-16 02:24:09 -0800

    Coded server.js code to get server set up and running. Pathed html pages using GET.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# note-taker
+# <note-taker>
+# **Note Taker**
+
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+<!-- ![Express](https://img.shields.io/badge/Jest-C21325?style=for-the-badge&logo=jest&logoColor=white) -->
+
+## **Description**
+
+ 
+
+## **Installation & Usage**
+
+
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { Server } = require('http');
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => 
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+app.post('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      const parsedNotes = JSON.parse(data);
+      res.status(200).json(parsedNotes);
+    }
+  })
+})
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
